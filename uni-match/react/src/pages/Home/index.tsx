ea3b8815c8ee83022c0aa028730233a864e6771c
Christopher Hu <40853270+christopher192@users.noreply.github.com>
2024-05-26 17:27:56 +0800

    update

--- uni-match/react/src/pages/Home/index.tsx
+++ uni-match/react/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
 import { 
   Card,
@@ -15,236 +15,60 @@ import {
 import Flatpickr from "react-flatpickr";
 import BreadCrumb from "../../Components/Common/BreadCrumb";
 import { jobGrid } from "../../common/data/appsJobs";
-import { useSpring, animated } from '@react-spring/web'
-import { useDrag } from '@use-gesture/react'
+import { useSpring, animated, to } from '@react-spring/web'
+import { useGesture, useDrag } from '@use-gesture/react'
+import useMeasure from 'react-use-measure'
+import styles from './styles.module.css'
 
 const Home = () => {
   document.title = "Home";
 
+  const [showElement, setShowElement] = useState(true);
+  const [{ border, width, height, touchAction, x, y }, api] = useSpring(() => ({ border: '1px solid black', width: '250px', height: '250px', touchAction: 'none',  x: 0, y: 0 }));
+  const bind = useGesture({
+    onDrag: ({ down, movement: [mx, my], velocity }) => { 
+      api.start({ x: down ? mx : 0, y: down ? my : 0, immediate: down });
+
+      // ignore swipe up and down
+      if (Math.abs(my) > Math.abs(mx)) {
+        return;
+      }
+
+      const isSwipeLeft = velocity[0] > 0.5 && mx < 0;
+      const isSwipeRight = velocity[0] > 0.5 && mx > 0;
+
+      if (isSwipeLeft) {
+        console.log('Swipe left detected');
+      } else if (isSwipeRight) {
+        console.log('Swipe right detected');
+      }
+
+      // drag has stopped
+      if (!down) {
+        console.log('Drag has stopped');
+        // hide the element
+        setShowElement(false);
+      }
+    },
+  });
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <React.Fragment>
       <div className = "page-content">
-        <div className="container-fluid">
+        <div className = "container-fluid">
           <BreadCrumb title = "Recommand List" pageTitle = "Dashboard" />
           <Row>
-            <Col lg={12}>
-              <Card>
-                <CardBody className="bg-light-subtle">
-                  <Form>
-                    <Row className="g-3">
-                      <Col xxl={4} sm={12}>
-                        <div className="search-box">
-                          <Input
-                            type="text"
-                            className="form-control search bg-light border-light"
-                            id="searchJob"
-                            autoComplete="off"
-                            placeholder="Search for jobs or companies..."
-                          />
-                          <i className="ri-search-line search-icon"></i>
-                        </div>
-                      </Col>
-
-                      <Col xxl={3} sm={4}>
-                        <Flatpickr
-                          className="form-control"
-                          id="datepicker-publish-input"
-                          placeholder="Select a date"
-                          options={{
-                            altInput: true,
-                            altFormat: "F j, Y",
-                            mode: "multiple",
-                            dateFormat: "d.m.y",
-                          }}
-                        />
-                      </Col>
-
-                      <Col xxl={2} sm={4}>
-                        <div className="input-light">
-                          <select
-                            className="form-control"
-                            data-choices
-                            data-choices-search-false
-                            name="choices-idType"
-                            id="idType"
-                          >
-                            <option value="all" defaultValue=''>
-                              All
-                            </option>
-                            <option value="Full Time">Full Time</option>
-                            <option value="Part Time">Part Time</option>
-                            <option value="Intership">Intership</option>
-                            <option value="Freelance">Freelance</option>
-                          </select>
-                        </div>
-                      </Col>
-
-                      <Col xxl={2} sm={4}>
-                        <div className="input-light">
-                          <select
-                            className="form-control"
-                            data-choices
-                            data-choices-search-false
-                            name="choices-single-default"
-                            id="idStatus"
-                          >
-                            <option value="all" defaultValue=''>
-                              All
-                            </option>
-                            <option value="Active">Active</option>
-                            <option value="New">New</option>
-                            <option value="Close">Close</option>
-                          </select>
-                        </div>
-                      </Col>
-
-                      <Col xxl={1} sm={4}>
-                        <button
-                          type="button"
-                          className="btn btn-primary w-100"
-                          //   onclick="filterData();"
-                        >
-                          <i className="ri-equalizer-fill me-1 align-bottom"></i>{" "}
-                          Filters
-                        </button>
-                      </Col>
-                    </Row>
-                  </Form>
-                </CardBody>
-              </Card>
+            <Col lg = {6}>
+              {showElement && (
+                <animated.div style = {{ border, width, height, touchAction , x, y }} 
+                  {...bind()}>
+                </animated.div>
+              )}
             </Col>
-          </Row>
-
-          <Row>
-            <Col lg={12}>
-              <div className="d-flex align-items-center mb-4">
-                <div className="flex-grow-1">
-                  <p className="text-muted fs-14 mb-0">
-                    Result: <span id="total-result">7</span>
-                  </p>
-                </div>
-                <div className="flex-shrink-0">
-                  <UncontrolledDropdown className="dropdown">
-                    <DropdownToggle
-                      className="btn text-muted fs-14 dropdown-toggle"
-                      to="#"
-                      role="button"
-                      tag="button"
-                      type="button"
-                    >
-                      All View
-                    </DropdownToggle>
-                    <DropdownMenu
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuLink"
-                    >
-                      <li>
-                        <DropdownItem className="dropdown-item" href="#">
-                          Action
-                        </DropdownItem>
-                      </li>
-                      <li>
-                        <DropdownItem className="dropdown-item" href="#">
-                          Another action
-                        </DropdownItem>
-                      </li>
-                      <li>
-                        <DropdownItem className="dropdown-item" href="#">
-                          Something else here
-                        </DropdownItem>
-                      </li>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </div>
-              </div>
-            </Col>
-          </Row>
-
-          <Row id="job-list">
-          <Col lg={3} md={6} id="job-widget">
-              <Card className="card-height-100 bg-info bg-job">
-                <CardBody className="p-5">
-                  <h2 className="lh-base text-white">
-                    Velzon invites young professionals for an intership!
-                  </h2>
-                  <p className="text-white text-opacity-75 mb-0 fs-14">
-                    Don't miss your opportunity to improve your skills!
-                  </p>
-                  <div className="mt-5 pt-2">
-                    <button type="button" className="btn btn-light w-100">
-                      View More{" "}
-                      <i className="ri-arrow-right-line align-bottom"></i>
-                    </button>
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-            {jobGrid.map((item, key) => (
-              <Col lg={3} md={6} key={key}>
-                <Card>
-                  <CardBody>
-                    <button
-                      type="button"
-                      className="btn btn-icon btn-soft-primary float-end"
-                      data-bs-toggle="button"
-                      aria-pressed="true"
-                    >
-                      <i className="mdi mdi-cards-heart fs-16"></i>
-                    </button>
-                    <div className="avatar-sm mb-4">
-                      <div className="avatar-title bg-light rounded">
-                        <img
-                          src={item.companyLogo}
-                          alt=""
-                          className="avatar-xxs"
-                        />
-                      </div>
-                    </div>
-                    <Link to="#">
-                      <h5>{item.jobTitle}</h5>
-                    </Link>
-                    <p className="text-muted">{item.companyName} </p>
-                    <div className="d-flex gap-4 mb-3">
-                      <div>
-                        <i className="ri-map-pin-2-line text-primary me-1 align-bottom"></i>{" "}
-                        {item.location}
-                      </div>
-                      <div>
-                        <i className="ri-time-line text-primary me-1 align-bottom"></i>{" "}
-                        {item.postDate}
-                      </div>
-                    </div>
-                    <p className="text-muted">{item.description}</p>
-                    <div className="hstack gap-2">
-                      {item.requirement.map((subItem, key) => (
-                        <React.Fragment key={key}>
-                          {
-                            subItem === "Full Time" ?
-                              <span className="badge bg-success-subtle text-success">{subItem}</span>
-                              :
-                              subItem === "Freelance" ?
-                                <span className="badge bg-primary-subtle text-primary">{subItem}</span>
-                                :
-                                <span className="badge bg-danger-subtle  text-danger">{subItem}</span>
-                          }
-                        </React.Fragment>
-                      ))}
-                    </div>
-                    <div className="mt-4 hstack gap-2">
-                      <Link to="#" className="btn btn-soft-primary w-100">
-                        Apply Job
-                      </Link>
-                      <Link
-                        to="/apps-job-details"
-                        className="btn btn-soft-success w-100"
-                      >
-                        Overview
-                      </Link>
-                    </div>
-                  </CardBody>
-                </Card>
-              </Col>
-            ))}
           </Row>
         </div>
       </div>
