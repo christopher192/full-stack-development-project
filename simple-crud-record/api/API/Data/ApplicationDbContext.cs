de6f6fa05792d8c43cbc310cb8be43f5b6cac73f
Christopher Hu <40853270+christopher192@users.noreply.github.com>
2024-05-29 21:56:21 +0800

    update

---
+++ simple-crud-record/api/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Data.Models;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Record> Cities => Set<Record>();
+    }
+}
