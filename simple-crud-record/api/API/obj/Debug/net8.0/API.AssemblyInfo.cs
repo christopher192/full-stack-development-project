3232549893ddab63723b925d2d08425b9517ae40
Christopher Hu <40853270+christopher192@users.noreply.github.com>
2024-06-27 17:25:37 +0800

    update

--- simple-crud-record/api/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ simple-crud-record/api/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe814b7679b6f9345a856552e4b00423d05cb84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5021b71ed8ba61f0ab1af21c3736f97676d1fa30")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
